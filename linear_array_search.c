60aa351c01189eb128e90840146c0dbbe07be892
Monte-Kristo <83087001+RATTER95292@users.noreply.github.com>
2022-11-23 11:06:58 +0300

    Преобразил программу .

--- linear_array_search.c
+++ linear_array_search.c
@@ -4,36 +4,47 @@
 
 #define MAXN 100
 
-void vvodMassiva(int *a,int n){
-    int i;
-    for(i=0;i<n;i++)
+//функция для ввода массив
+void vvodMassiva(int *a,int n);
+//функция для вывода массива
+void print(int C[], int size);
+//функция для поиска повторяющихся элементов массива
+int find_in_massive(int A[],int m, int a, int d);
+//функция поиск элемнтов массива в другом массива изапись их в другом массиве
+int search(int C[], int A[], int B[], int n, int m );
+
+int main()
+{
+    int A[MAXN];
+    int B[MAXN];
+    int C[MAXN];
+    int n,m;
+
+    printf("Enter n and m (1-%d) ",MAXN);
+    scanf("%d",&n);
+    scanf("%d",&m);
+    if((n<1 || n>MAXN) || (m<1 || m>MAXN))
     {
-        printf("Enter a[%d]=",i+1);
-        scanf("%d",&a[i]);
+        printf("Error! a<1 or b>%d",MAXN);
+        getch();
+        return 1;
     }
-}
+    
+    printf("Enter an array А\n");
+    vvodMassiva(A,n);
+    printf("Enter an array В\n");
+    vvodMassiva(B,m);
+    
+    int size = search(C,A,B,n,m);
 
-void print(int C[], int size){
-    for(int i=0;i<size;i++){
-        printf("%d ",C[i]);
-    }
-    printf("\n");
-}
+    printf("Array А\n");
+    print(A,n);
+    printf("Array В\n");
+    print(B,n);
+    printf("Itog array\n");
+    print(C,size);
 
-
-int find_in_massive(int A[],int m, int a, int d){
-    int i;
-    int count = 0;
-    for (i =0;i<m;i++){
-        if(a == A[i]){
-            count++;
-        }
-    } 
-    if(count == d) {
-    return 1;
-    }else{ 
     return 0;
-    }
 }
 
 int search(int C[], int A[], int B[], int n, int m ){
@@ -50,37 +61,34 @@ int search(int C[], int A[], int B[], int n, int m ){
     return k;
 }
 
-int main()
-{
-    printf("Program start");
-    int A[MAXN];
-    int B[MAXN];
-    int C[MAXN];
-    int n,m;
-
-    printf("Enter n and m (1-%d) ",MAXN);
-    scanf("%d",&n);
-    scanf("%d",&m);
-    if((n<1 || n>MAXN) || (m<1 || m>MAXN))
-    {
-        printf("Error! a<1 or b>%d",MAXN);
-        getch();
-        return 1;
-    }
-    
-    printf("Введите массив А\n");
-    vvodMassiva(A,n);
-    printf("Введите массив В\n");
-    vvodMassiva(B,m);
-    
-    int size = search(C,A,B,n,m);
-
-    printf("Массив А\n");
-    print(A,n);
-    printf("Массив В\n");
-    print(B,n);
-    printf("itog\n");
-    print(C,size);
-
+int find_in_massive(int A[],int m, int a, int d){
+    int i;
+    int count = 0;
+    for (i =0;i<m;i++){
+        if(a == A[i]){
+            count++;
+        }
+    } 
+    if(count == d) {
+    return 1;
+    }else{ 
     return 0;
+    }
 }
+
+void print(int C[], int size){
+    for(int i=0;i<size;i++){
+        printf("%d ",C[i]);
+    }
+    printf("\n");
+}
+
+void vvodMassiva(int *a,int n){
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("Enter a[%d]=",i+1);
+        scanf("%d",&a[i]);
+    }
+}
+

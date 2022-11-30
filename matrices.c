0c7a15bce8af7a23eb158b6140dcda618b30d74c
Monte-Kristo <83087001+RATTER95292@users.noreply.github.com>
2022-11-30 18:54:42 +0300

    Update matrices.c

--- matrices.c
+++ matrices.c
@@ -8,6 +8,9 @@
 //Функция для ввода матрицы
 void importMatrix(int matrix[ROWS][COLS]);
 
+//Вывод матрицы
+void printMatrix(int matrix[ROWS][COLS], char quest1[4][30], char quest2[5][30]);
+
 //Функция для подсяета количество гостей
 int maxOwner(int matrix[ROWS][ROWS]);
 
@@ -16,27 +19,14 @@ int maxQuest(int matrix[ROWS][ROWS]);
 
 void main(){
     
-    const char *quest2[] = {"Камилла","Артур  ","Боб    ","Мария  "};
-    const char *quest1[] = {"хозяин/гость","Камилла","Артур","Боб","Мария"};
+    char quest2[4][30] = {"Камилла","Артур  ","Боб    ","Мария  "};
+    char quest1[5][30] = {"хозяин/гость","Камилла ","Артур","Боб","Мария"};
     int a[4][4];                     
 
     importMatrix(a);
 
-    //Вывод матрицы
-    for (int i=0; i<4; i++){
-        if(i == 0){
-            for(int k = 0; k < 5; k++) printf(" %-15s",quest1[k]);
-            printf("\n");
-        }
-        printf(" %s", quest2[i]);
-        for (int j=0; j<4; j++){ 
-            if(j == i){
-                printf("         -");
-            }else{
-            printf("%10d",a[i][j]);}}             
-        printf("\n");                                         
-    }
-    
+    printMatrix(a, quest1, quest2);
+
     printf("\nБольше всего гостей принял(а) %s\n", quest1[maxOwner(a)+1]);
     printf("Больше всего в гостях был(а) %s\n",quest1[maxQuest(a)+1]);
     
@@ -56,7 +46,21 @@ void importMatrix(int matrix[ROWS][COLS]){
     }
     printf("\n");
 }
-
+void printMatrix(int matrix[ROWS][COLS], char quest1[4][30], char quest2[5][30]){
+    for (int i=0; i<4; i++){
+        if(i == 0){
+            for(int k = 0; k < 5; k++) printf(" %-15s",quest1[k]);
+            printf("\n");
+        }
+        printf(" %s", quest2[i]);
+        for (int j=0; j<4; j++){ 
+            if(j == i){
+                printf("         -");
+            }else{
+            printf("%10d",matrix[i][j]);}}             
+        printf("\n");                                         
+    }
+}
 int maxQuest(int matrix[ROWS][ROWS]){
     int q =0;
     int q1 = 0;
